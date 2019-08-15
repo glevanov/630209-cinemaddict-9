d14d248e9c4d18369a3e5a5c7e4303a1673e8ff0
Grigoriy Levanov <glevanov@gmail.com>
2019-08-15 22:34:23 +0300

    feat(components): update Sort data signature

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -6,20 +6,20 @@ export const getSort = () => {
   const sortTypes = [
     {
       title: `Sort by default`,
-      active: true,
+      isActive: true,
     },
     {
       title: `Sort by date`,
-      active: false,
+      isActive: false,
     },
     {
       title: `Sort by rating`,
-      active: false,
+      isActive: false,
     },
   ];
 
   const sortItems = sortTypes.map((item) => `
-    <li><a href="#" class="sort__button ${(item.active) ? `sort__button--active` : ``}">${item.title}</a></li>
+    <li><a href="#" class="sort__button ${(item.isActive) ? `sort__button--active` : ``}">${item.title}</a></li>
   `);
 
   return `
