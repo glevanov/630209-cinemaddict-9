4eec710d72469550bf7dfddf0164bd778cd5c4c1
Grigoriy Levanov <glevanov@gmail.com>
2019-08-15 22:08:40 +0300

    feat(components): extract data from Sort markup

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,9 +2,28 @@
  * Returns Sort element markup
  * @return {string} element markup
  */
-export const getSort = () => `
-<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
-</ul>`;
+export const getSort = () => {
+  const sortTypes = [
+    {
+      title: `Sort by default`,
+      active: true,
+    },
+    {
+      title: `Sort by date`,
+      active: false,
+    },
+    {
+      title: `Sort by rating`,
+      active: false,
+    },
+  ];
+
+  const sortItems = sortTypes.map((item) => `
+    <li><a href="#" class="sort__button ${(item.active) ? `sort__button--active` : ``}">${item.title}</a></li>
+  `);
+
+  return `
+  <ul class="sort">
+    ${sortItems.join(``)}
+  </ul>`;
+};
